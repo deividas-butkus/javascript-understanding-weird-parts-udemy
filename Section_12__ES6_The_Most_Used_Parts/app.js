a2003672fd4916c613df358d91adb28f07e215ac
deividas-butkus <deividas.butkus@hotmail.com>
2024-12-04 16:26:34 +0200

    Section 12: ES6 - The Most Used Parts - 3 - Arrow Functions

--- Section_12__ES6_The_Most_Used_Parts/app.js
+++ Section_12__ES6_The_Most_Used_Parts/app.js
@@ -17,15 +17,50 @@
 
 // console.log(myVar);
 
-//const
+// //const
 
-const MY_VAR = 1;
-const greet = { name: "Dave" };
+// const MY_VAR = 1;
+// const greet = { name: "Dave" };
 
-{
-  const greet = { name: "Dave", lastname: "Butkus" };
-  greet.name = "Deividas";
-  console.log(greet);
+// {
+//   const greet = { name: "Dave", lastname: "Butkus" };
+//   greet.name = "Deividas";
+//   console.log(greet);
+// }
+
+// console.log(greet);
+
+// Arrow Functions
+
+function greeter(name) {
+  return "Hello " + name;
+}
+console.log(greeter("Dave"));
+
+const greeterArrow = (name) => "Hello " + name;
+console.log(greeterArrow("Dave"));
+
+function greet(greetFunc, name) {
+  return greetFunc(name);
 }
 
-console.log(greet);
+console.log(greet((name) => "Hi " + name, "Deividas"));
+
+// function Timer() {
+//   this.seconds = 0;
+//   setInterval(() => {
+//     this.seconds++;
+//     console.log(this.seconds);
+//   }, 1000);
+// }
+
+// const timer = new Timer();
+
+const greeter2 = {
+  name: "Dave",
+  greet: function () {
+    return "Hello " + this.name;
+  },
+};
+
+console.log(greeter2.greet());
