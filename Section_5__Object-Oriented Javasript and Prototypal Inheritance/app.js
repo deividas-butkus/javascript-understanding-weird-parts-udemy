85e41d26dc97c35d028ebbfe6baab4ccc79f843d
deividas-butkus <deividas.butkus@hotmail.com>
2024-12-04 17:07:58 +0200

    Section 5: Object-Oriented-JavaScript - 1 - Understanding the Prototype

---
+++ Section_5__Object-Oriented Javasript and Prototypal Inheritance/app.js
@@ -0,0 +1,27 @@
+const person = {
+  firstname: "Default",
+  lastname: "Default",
+  getFullName: function () {
+    return this.firstname + " " + this.lastname;
+  },
+};
+
+console.log(person.getFullName());
+
+const john = {
+  firstname: "John",
+  lastname: "Doe",
+};
+
+// ! DON"T DO THIS ever - for demo purposes only
+john.__proto__ = person;
+
+console.log(john.getFullName());
+console.log(john.firstname);
+
+const jane = {
+  firstname: "Jane",
+};
+
+jane.__proto__ = person;
+console.log(jane.getFullName());
