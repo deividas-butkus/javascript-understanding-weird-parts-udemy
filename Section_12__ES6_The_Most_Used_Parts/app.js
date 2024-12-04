ceffc630b41927e3ad43165bbea1d090e11308da
deividas-butkus <deividas.butkus@hotmail.com>
2024-12-04 14:47:15 +0200

    Section 12: ES6 - The Most Used Parts - 1 - const

--- Section_12__ES6_The_Most_Used_Parts/app.js
+++ Section_12__ES6_The_Most_Used_Parts/app.js
@@ -1,18 +1,31 @@
-let myVar = 1;
 // let
 
+// let myVar = 1;
+// {
+//   let myVar = 2;
+//   console.log(myVar);
+
+//   {
+//     let myVar = 3;
+
+//     for (let myVar = 4; myVar > 3; myVar--) {
+//       console.log(myVar);
+//     }
+//     console.log(myVar);
+//   }
+// }
+
+// console.log(myVar);
+
+//const
+
+const MY_VAR = 1;
+const greet = { name: "Dave" };
+
 {
-  let myVar = 2;
-  console.log(myVar);
-
-  {
-    let myVar = 3;
-
-    for (let myVar = 4; myVar > 3; myVar--) {
-      console.log(myVar);
-    }
-    console.log(myVar);
-  }
+  const greet = { name: "Dave", lastname: "Butkus" };
+  greet.name = "Deividas";
+  console.log(greet);
 }
 
-console.log(myVar);
+console.log(greet);
