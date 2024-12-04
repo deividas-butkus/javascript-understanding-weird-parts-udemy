789f242dff400a437b34380ed31aae1a61c80962
deividas-butkus <deividas.butkus@hotmail.com>
2024-12-04 09:20:42 +0200

    Section 4: Objects and Functions - 2

--- Section_4__Objects_and_Dot/app.js
+++ Section_4__Objects_and_Dot/app.js
@@ -1,203 +1,241 @@
-const dave = new Object();
+// const dave = new Object();
 
-dave["firstname"] = "Dave";
-dave["lastname"] = "Butkus";
+// dave["firstname"] = "Dave";
+// dave["lastname"] = "Butkus";
 
-const firstNameProperty = "firstname";
+// const firstNameProperty = "firstname";
 
-console.log(dave);
-console.log(dave[firstNameProperty]);
+// console.log(dave);
+// console.log(dave[firstNameProperty]);
 
-console.log(dave.firstname);
-console.log(dave.lastname);
+// console.log(dave.firstname);
+// console.log(dave.lastname);
 
-dave.address = new Object();
-dave.address.street = "Žukausko g.";
-dave.address.city = "Vilnius";
-dave.address.region = "Vilniaus apskritis";
+// dave.address = new Object();
+// dave.address.street = "Žukausko g.";
+// dave.address.city = "Vilnius";
+// dave.address.region = "Vilniaus apskritis";
 
-console.log(dave.address.street);
-console.log(dave.address.city);
-console.log(dave["address"]["region"]);
+// console.log(dave.address.street);
+// console.log(dave.address.city);
+// console.log(dave["address"]["region"]);
 
-const person1 = {
-  firstname: "Dave",
-  lastname: "Butkus",
-  address: {
-    street: "Žukausko g.",
-    city: "Vilnius",
-    region: "Vilniaus apskritis",
-  },
-};
-
-console.log(person1);
-
-// const person3 = new Object({
-//   fisrtname: "Dave",
+// const person1 = {
+//   firstname: "Dave",
 //   lastname: "Butkus",
-// });
+//   address: {
+//     street: "Žukausko g.",
+//     city: "Vilnius",
+//     region: "Vilniaus apskritis",
+//   },
+// };
 
-// console.log(person3);
+// console.log(person1);
 
-function greet(person) {
-  console.log(`Hi ${person.firstname}, you live on ${person.address.street}`);
-}
+// // const person3 = new Object({
+// //   fisrtname: "Dave",
+// //   lastname: "Butkus",
+// // });
 
-greet(dave);
+// // console.log(person3);
 
-const belzeb = { firstname: "Belzeb" };
-
-belzeb.address = {
-  street: "Gedimino pr.",
-};
-
-greet(belzeb);
-
-// Funtrions are objects
-function greetFn() {
-  console.log("Hi");
-}
-
-greetFn.language = "english";
-
-console.log(greetFn.language);
-
-let a;
-
-const anonymousGreet = function () {
-  console.log("Hi");
-};
-
-anonymousGreet();
-
-// function log(a) {
-//   console.log(a);
+// function greet(person) {
+//   console.log(`Hi ${person.firstname}, you live on ${person.address.street}`);
 // }
 
-function log(a) {
-  a();
-}
+// greet(dave);
 
-log(function () {
-  console.log("hi");
-});
+// const belzeb = { firstname: "Belzeb" };
 
-//by value
+// belzeb.address = {
+//   street: "Gedimino pr.",
+// };
 
-let v = 3;
-let z;
+// greet(belzeb);
 
-z = v;
-v = 5;
+// // Funtrions are objects
+// function greetFn() {
+//   console.log("Hi");
+// }
 
-console.log(v); // 5
-console.log(z); // 3
+// greetFn.language = "english";
 
-// by reference (all objects, incl. fns)
-const vObj = { firstname: "Dave" };
-let zObj;
+// console.log(greetFn.language);
 
-zObj = vObj;
+// let a;
 
-console.log(vObj);
-console.log(zObj);
+// const anonymousGreet = function () {
+//   console.log("Hi");
+// };
 
-vObj.firstname = "Dave The 2nd";
+// anonymousGreet();
 
-console.log(vObj);
-console.log(zObj);
+// // function log(a) {
+// //   console.log(a);
+// // }
 
-// by reference (even as parameters)
-function changeFirstname(obj) {
-  obj.firstname = "Peter";
-}
+// function log(a) {
+//   a();
+// }
 
-changeFirstname(zObj);
-console.log(vObj);
-console.log(zObj);
+// log(function () {
+//   console.log("hi");
+// });
 
-const string = "Laba diena";
+// //by value
 
-console.log(string.toUpperCase());
-console.log(string);
+// let v = 3;
+// let z;
 
-// quals operator set up a new memory space
-zObj = { firstname: "Anupras" };
-console.log(vObj);
-console.log(zObj);
+// z = v;
+// v = 5;
 
-// this
+// console.log(v); // 5
+// console.log(z); // 3
 
-function aThis() {
-  console.log(this);
-}
+// // by reference (all objects, incl. fns)
+// const vObj = { firstname: "Dave" };
+// let zObj;
 
-let bThis = () => console.log(this);
+// zObj = vObj;
 
-aThis();
-bThis();
+// console.log(vObj);
+// console.log(zObj);
 
-const cObjThis = {
-  name: "The c object",
-  log: function () {
-    this.name = "Updated C object";
-    console.log(this);
+// vObj.firstname = "Dave The 2nd";
+
+// console.log(vObj);
+// console.log(zObj);
+
+// // by reference (even as parameters)
+// function changeFirstname(obj) {
+//   obj.firstname = "Peter";
+// }
+
+// changeFirstname(zObj);
+// console.log(vObj);
+// console.log(zObj);
+
+// const string = "Laba diena";
+
+// console.log(string.toUpperCase());
+// console.log(string);
+
+// // quals operator set up a new memory space
+// zObj = { firstname: "Anupras" };
+// console.log(vObj);
+// console.log(zObj);
+
+// // this
+
+// function aThis() {
+//   console.log(this);
+// }
+
+// let bThis = () => console.log(this);
+
+// aThis();
+// bThis();
+
+// const cObjThis = {
+//   name: "The c object",
+//   log: function () {
+//     this.name = "Updated C object";
+//     console.log(this);
+//   },
+// };
+
+// let name = "Global scope name";
+
+// const cObjThisArrow = {
+//   name: "The c object",
+//   log: () => {
+//     console.log(this);
+//   },
+// };
+
+// cObjThis.log();
+// cObjThisArrow.log();
+
+// const reverseAndCheck = (str) => {
+//   return str === str.split("").reverse().join("");
+// };
+
+// console.log(reverseAndCheck("kedes"));
+// console.log(reverseAndCheck("abccba"));
+// console.log(reverseAndCheck("abcba"));
+
+// const cart = [
+//   { name: "apples", price: 1, qty: 10 },
+//   { name: "bananas", price: 2, qty: 20 },
+//   { name: "cabages", price: 0.5, qty: 10 },
+// ];
+
+// const cartSum = cart.reduce((acc, cur) => {
+//   return acc + cur.price * cur.qty;
+// }, 0);
+
+// console.log(cartSum);
+
+// let sum = 0;
+// cart.forEach((obj) => (sum += obj.price * obj.qty));
+
+// console.log(sum);
+
+// console.log([] == "");
+// console.log([] === "");
+
+// console.log("2" == 2);
+// console.log("2" === 2);
+
+// console.log("2" + 2);
+// console.log(2 + 2);
+// console.log("2" * 2);
+
+// console.log(3 > 2 > 1);
+// console.log(true > 1);
+// console.log(Number(true));
+
+// console.log(1 < 2 < 3);
+// console.log(true < 3);
+
+// console.log(3 > 2 == 1);
+// console.log(3 > 2 === 1);
+
+// Arrays, fn arguments vs. default values and ...rest
+
+const arr = [
+  1,
+  false,
+  { name: "Dave", address: "Žukausko g. 17-5" },
+  function (name) {
+    let greeting = "Hello ";
+    console.log(greeting + name);
   },
-};
-
-let name = "Global scope name";
-
-const cObjThisArrow = {
-  name: "The c object",
-  log: () => {
-    console.log(this);
-  },
-};
-
-cObjThis.log();
-cObjThisArrow.log();
-
-const reverseAndCheck = (str) => {
-  return str === str.split("").reverse().join("");
-};
-
-console.log(reverseAndCheck("kedes"));
-console.log(reverseAndCheck("abccba"));
-console.log(reverseAndCheck("abcba"));
-
-const cart = [
-  { name: "apples", price: 1, qty: 10 },
-  { name: "bananas", price: 2, qty: 20 },
-  { name: "cabages", price: 0.5, qty: 10 },
+  "hello",
 ];
 
-const cartSum = cart.reduce((acc, cur) => {
-  return acc + cur.price * cur.qty;
-}, 0);
+console.log(arr);
+arr[3](arr[2].name);
+arr[3](arr[2]["name"]);
 
-console.log(cartSum);
+function greet(firstname, lastname, language = "English", ...rest) {
+  //   language = language || "English";
 
-let sum = 0;
-cart.forEach((obj) => (sum += obj.price * obj.qty));
+  if (!arguments.length) {
+    console.log("Missing params");
+    console.log("----------------");
+    return;
+  }
+  console.log(firstname);
+  console.log(lastname);
+  console.log(language);
+  console.log(arguments);
+  console.log("---------------");
+}
 
-console.log(sum);
-
-console.log([] == "");
-console.log([] === "");
-
-console.log("2" == 2);
-console.log("2" === 2);
-
-console.log("2" + 2);
-console.log(2 + 2);
-console.log("2" * 2);
-
-console.log(3 > 2 > 1);
-console.log(true > 1);
-console.log(Number(true));
-
-console.log(1 < 2 < 3);
-console.log(true < 3);
-
-console.log(3 > 2 == 1);
-console.log(3 > 2 === 1);
+greet();
+greet("Dave");
+greet("Dave", "Butkus");
+greet("Dave", "Butkus", "Lithuanian");
+greet("Dave", "Butkus", "Lithuanian", 10, false, "belekas");
