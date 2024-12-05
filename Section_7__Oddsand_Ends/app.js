203ff18f5191714eb2b7b030588d733cb820d668
deividas-butkus <deividas.butkus@hotmail.com>
2024-12-05 15:31:09 +0200

    Section 7: Odds and Ends - 1 - Initialization

---
+++ Section_7__Oddsand_Ends/app.js
@@ -0,0 +1,22 @@
+// Initialization
+
+const people = [
+  {
+    // the John object
+    firstname: "John",
+    lastname: "Doe",
+    addresses: ["111 Main St.", "222 Third St."],
+  },
+  {
+    // the Jane object
+    firstname: "Jane",
+    lastname: "Doe",
+    addresses: ["333 Main St.", "444 Fifth St."],
+    greet: function () {
+      return "Hello";
+    },
+  },
+];
+
+console.log(people);
+console.log(people[1].greet());
