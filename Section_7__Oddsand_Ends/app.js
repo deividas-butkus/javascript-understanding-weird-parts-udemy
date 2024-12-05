4bfa6abcb64eab4399f01bda6bc75df8e5fb2d95
deividas-butkus <deividas.butkus@hotmail.com>
2024-12-05 15:48:54 +0200

    Section 7: Odds and Ends - 2 - 'typeof' , 'instanceof', and Figuring Out What Something Is

--- Section_7__Oddsand_Ends/app.js
+++ Section_7__Oddsand_Ends/app.js
@@ -18,5 +18,34 @@ const people = [
   },
 ];
 
-console.log(people);
-console.log(people[1].greet());
+// console.log(people);
+// console.log(people[1].greet());
+
+// 'typeof' , 'instanceof', and Figuring Out What Something Is
+
+var a = 3;
+console.log(typeof a);
+
+const b = "Hello";
+console.log(typeof b);
+
+const c = {};
+console.log(typeof c);
+
+const d = [];
+console.log(typeof d); // weird!
+console.log(Object.prototype.toString.call(d)); // better!
+
+function Person(name) {
+  this.name = name;
+}
+
+const e = new Person("Jane");
+console.log(typeof e);
+console.log(e instanceof Person);
+
+console.log(typeof undefined); // makes sense
+console.log(typeof null); // a bug since, like, forever...
+
+const z = function () {};
+console.log(typeof z);
