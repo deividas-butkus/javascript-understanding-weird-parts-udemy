dc9f046c9c858ac026dbeb9aff4c3c3f44703bf8
deividas-butkus <deividas.butkus@hotmail.com>
2024-12-05 12:54:11 +0200

    Section 5: Object-Oriented JavaScript and Prototypal Inheritance - 3 - Reflection and Extend

--- Section_5__Object-Oriented Javasript and Prototypal Inheritance/app.js
+++ Section_5__Object-Oriented Javasript and Prototypal Inheritance/app.js
@@ -30,6 +30,49 @@
 
 // Everything is an Object )or a Primitive)
 
-const a = {};
-const b = function () {};
-const c = [];
+// const a = {};
+// const b = function () {};
+// const c = [];
+
+// Reflection and Extend
+
+const person = {
+  firstname: "Default",
+  lastname: "Default",
+  getFullName: function () {
+    return this.firstname + " " + this.lastname;
+  },
+};
+
+const john = {
+  firstname: "John",
+  lastname: "Doe",
+};
+
+for (const prop in john) {
+  if (john.hasOwnProperty(prop)) {
+    console.log(prop + ": " + john[prop]);
+  }
+}
+
+const jane = {
+  adsress: "111 Main str",
+  getFormalFullName: function () {
+    return this.lastname + ", " + this.firstname;
+  },
+};
+
+const jim = {
+  getFirstName: function () {
+    return this.firstname;
+  },
+};
+
+// _.extend(john, jane, jim);
+
+// console.log(john);
+// console.log(john.getFormalFullName());
+// console.log(john.getFirstName());
+
+Object.assign(john, jane, jim);
+console.log(john);
